deaabc48a919bc051c4f92948eac4e254dd9dc76
colleenjoyce <colleenelizabethjoyce@gmail.com>
2014-03-03 16:41:13 -0500

    config edit

--- config/application.rb
+++ config/application.rb
@@ -11,11 +11,10 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
 
-config.assets.initialize_on_precompile = false
-
-
 module Twitterick
   class Application < Rails::Application
+
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
