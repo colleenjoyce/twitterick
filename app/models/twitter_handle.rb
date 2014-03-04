cb28190ef5f5c8bc402d6f9bde18836e13e01281
Isaac50 <irt007@gmail.com>
2014-03-04 11:09:22 -0500

    downcase handle

--- app/models/twitter_handle.rb
+++ app/models/twitter_handle.rb
@@ -2,5 +2,12 @@ class TwitterHandle < ActiveRecord::Base
 	has_many :tweets 
 	belongs_to :user 
 
-	validates :handle, uniqueness: { case_sensitive: false }, presence: true 
+	validates :handle, uniqueness: true, presence: true 
+
+	before_validation :downcase_handle
+
+	private
+	def downcase_handle
+		self.handle = self.handle.downcase
+	end
 end
